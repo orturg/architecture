482795ef631cdb2547486c0f3b0f8d65fd1b81da
orturg <a.nozhenko@ukma.edu.ua>
2024-03-13 15:32:58 +0200

    User can enter numbers in console and program prints number that user typed

--- Architecture.asm
+++ Architecture.asm
@@ -1,7 +1,39 @@
 .model small
 .stack 100h
-   
+
+.data
+    input_msg db 10, 13, 'Enter numbers: $'
+    output_msg db 10, 13, 'You entered: $'
+    buffer db 255, ?, 255 dup('$') ; буфер для зберігання введеного рядка
+
 .code
-main PROC
-    mov AX word 4
-main ENDP
+    main PROC
+        mov ax, @data
+        mov ds, ax
+
+        ; Просимо у користувача ввести числа
+        mov ah, 09h
+        lea dx, input_msg
+        int 21h
+
+        ; Зчитуємо введений рядок
+        mov ah, 0Ah
+        lea dx, buffer
+        int 21h
+
+        ; Виводимо повідомлення про введений рядок
+        mov ah, 09h
+        lea dx, output_msg
+        int 21h
+
+        ; Виводимо введений рядок
+        mov ah, 09h
+        lea dx, buffer+2 
+        int 21h
+
+        mov ah, 4Ch    ; вихід із програми
+        int 21h
+    main ENDP
+
+END main
+
