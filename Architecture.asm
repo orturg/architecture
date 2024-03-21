717578266f6da7174f7517e2b075588412b52b16
orturg <a.nozhenko@ukma.edu.ua>
2024-03-22 01:32:41 +0200

    Programm prints numbers from array

--- Architecture.asm
+++ Architecture.asm
@@ -4,9 +4,9 @@
     input_msg db 10, 13, 'Enter numbers: $'
     space db ' ', '$'
     buffer db 1000 Dup(?)
-    numbers dw 5 Dup(?)
+    numbers dw 6 Dup(?)
     lengthArr dw ?
-    filename db 'test.txt', '$' 
+    ; filename db 'file1.txt', '$' 
 
     
 .code
@@ -16,6 +16,7 @@ start:
 
     mov ax, numbers
     call Input
+    call Output
 
     mov ax, 4C00h
     int 21h
@@ -30,7 +31,7 @@ start:
             mov ah, 0ah
             xor di, di
             mov dx, offset buffer ; 
-            int 21h ; зчитування строки
+            int 21h ; зчитування стрічки
 
             mov dl, 0ah
             mov ah, 02
@@ -47,8 +48,8 @@ start:
         convert :
             mov cl, [si] ; присвоюємо символ з буферу
 
-            cmp cl, 0dh  ; перевірка, чи символ не останній
-            jz end
+            cmp cl, 0dh  ; перевірка, чи не символ не останній
+            jz inputEnd
             
             cmp cl, ' ' ; перевіряємо, чи символ не є пробілом
             je addToArray
@@ -58,7 +59,7 @@ start:
             mul bx     ; перевід в 10
             add ax, cx  
             inc si    
-            jmp convert 
+            jmp convert     ; повторюємо
 
             xor cl, cl
             jmp convertNumbers
@@ -73,7 +74,7 @@ start:
             jmp convertNumbers
     
         ; запис останнього символа в масив, вихід із циклу переводу, задання значення довжини масиву
-        end:
+        inputEnd:
             xor cl, cl
             mov numbers[di], ax
             add di, 2
@@ -86,5 +87,66 @@ start:
             xor bx, bx
             ret
     Input endp
-    
+
+     
+
+
+    Output PROC
+        
+        xor dx, dx
+        xor ax, ax
+
+        xor di, di
+        xor si, si
+        mov si, lengthArr
+        printingNumbers:
+            mov ax, numbers[di]
+            call outputNum
+
+            increasingIndex:
+            call printSpace
+                add di, 2
+                dec si
+                cmp si, 0
+                jbe exit
+            loop printingNumbers
+
+        exit:
+            ret
+    Output endp
+     
+
+    outputNum PROC
+        firstStep:  
+                xor bx, bx
+                xor cx, cx
+                mov bx, 10 
+            secondStep:
+                xor dx, dx
+                div bx
+           
+                push dx
+                inc cx
+            
+                test ax, ax
+                jnz secondStep
+
+                mov ah, 02h
+            thirdStep:
+                pop dx
+                
+                add dl, '0'
+                int 21h
+                
+                loop thirdStep
+        ret
+    outputNum ENDP
+
+    printSpace PROC
+        mov ah, 09h
+        lea dx, space
+        int 21h
+        ret
+
+    printSpace ENDP
 end start
